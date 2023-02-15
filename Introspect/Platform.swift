fad0545a11fccf79a7963995dd27557bf51f0c7e
David Roman <2538074+davdroman@users.noreply.github.com>
2023-02-15 18:47:43 +0000

    wip

--- Introspect/Platform.swift
+++ Introspect/Platform.swift
@@ -20,137 +20,89 @@ enum Platform {
     var isCurrent: Bool {
         switch self {
         case .iOS(.v13):
-            #if os(iOS)
             if #available(iOS 14, *) {
                 return false
             }
             if #available(iOS 13, *) {
                 return true
             }
-            #else
-            return false
-            #endif
         case .iOS(.v14):
-            #if os(iOS)
             if #available(iOS 15, *) {
                 return false
             }
             if #available(iOS 14, *) {
                 return true
             }
-            #else
-            return false
-            #endif
         case .iOS(.v15):
-            #if os(iOS)
             if #available(iOS 16, *) {
                 return false
             }
             if #available(iOS 15, *) {
                 return true
             }
-            #else
-            return false
-            #endif
         case .iOS(.v16):
-            #if os(iOS)
             if #available(iOS 17, *) {
                 return false
             }
             if #available(iOS 16, *) {
                 return true
             }
-            #else
-            return false
-            #endif
         case .macOS(.v10_15):
-            #if os(macOS)
             if #available(macOS 11, *) {
                 return false
             }
             if #available(macOS 10.15, *) {
                 return true
             }
-            #else
-            return false
-            #endif
         case .macOS(.v11):
-            #if os(macOS)
             if #available(macOS 12, *) {
                 return false
             }
             if #available(macOS 11, *) {
                 return true
             }
-            #else
-            return false
-            #endif
         case .macOS(.v12):
-            #if os(macOS)
             if #available(macOS 13, *) {
                 return false
             }
             if #available(macOS 12, *) {
                 return true
             }
-            #else
-            return false
-            #endif
         case .macOS(.v13):
-            #if os(macOS)
             if #available(macOS 14, *) {
                 return false
             }
             if #available(macOS 13, *) {
                 return true
             }
-            #else
-            return false
-            #endif
         case .tvOS(.v13):
-            #if os(tvOS)
             if #available(tvOS 14, *) {
                 return false
             }
             if #available(tvOS 13, *) {
                 return true
             }
-            #else
-            return false
-            #endif
         case .tvOS(.v14):
-            #if os(tvOS)
             if #available(tvOS 15, *) {
                 return false
             }
             if #available(tvOS 14, *) {
                 return true
             }
-            #else
-            return false
-            #endif
         case .tvOS(.v15):
-            #if os(tvOS)
             if #available(tvOS 16, *) {
                 return false
             }
             if #available(tvOS 15, *) {
                 return true
             }
-            #else
-            return false
-            #endif
         case .tvOS(.v16):
-            #if os(tvOS)
             if #available(tvOS 17, *) {
                 return false
             }
             if #available(tvOS 16, *) {
                 return true
             }
-            #else
-            return false
-            #endif
         }
         return false
     }
--- IntrospectTests/PlatformTests.swift
+++ IntrospectTests/PlatformTests.swift
@@ -32,6 +32,7 @@ import XCTest
 
 final class PlatformTests: XCTestCase {
     func test_iOS() {
+        #if os(iOS)
         if #available(iOS 16, *) {
             XCTAssertEqual(Platform.iOS(.v16).isCurrent, true)
             XCTAssertEqual(Platform.iOS(.v15).isCurrent, false)
@@ -53,9 +54,16 @@ final class PlatformTests: XCTestCase {
             XCTAssertEqual(Platform.iOS(.v14).isCurrent, false)
             XCTAssertEqual(Platform.iOS(.v13).isCurrent, true)
         }
+        #else
+        XCTAssertEqual(Platform.iOS(.v16).isCurrent, false)
+        XCTAssertEqual(Platform.iOS(.v15).isCurrent, false)
+        XCTAssertEqual(Platform.iOS(.v14).isCurrent, false)
+        XCTAssertEqual(Platform.iOS(.v13).isCurrent, false)
+        #endif
     }
 
     func test_macOS() {
+        #if os(macOS)
         if #available(macOS 13, *) {
             XCTAssertEqual(Platform.macOS(.v13).isCurrent, true)
             XCTAssertEqual(Platform.macOS(.v12).isCurrent, false)
@@ -77,9 +85,16 @@ final class PlatformTests: XCTestCase {
             XCTAssertEqual(Platform.macOS(.v11).isCurrent, false)
             XCTAssertEqual(Platform.macOS(.v10_15).isCurrent, true)
         }
+        #else
+        XCTAssertEqual(Platform.macOS(.v13).isCurrent, false)
+        XCTAssertEqual(Platform.macOS(.v12).isCurrent, false)
+        XCTAssertEqual(Platform.macOS(.v11).isCurrent, false)
+        XCTAssertEqual(Platform.macOS(.v10_15).isCurrent, false)
+        #endif
     }
 
     func test_tvOS() {
+        #if os(tvOS)
         if #available(tvOS 16, *) {
             XCTAssertEqual(Platform.tvOS(.v16).isCurrent, true)
             XCTAssertEqual(Platform.tvOS(.v15).isCurrent, false)
@@ -101,12 +116,11 @@ final class PlatformTests: XCTestCase {
             XCTAssertEqual(Platform.tvOS(.v14).isCurrent, false)
             XCTAssertEqual(Platform.tvOS(.v13).isCurrent, true)
         }
-//        if #available(tvOS 9999, *) {
-//        } else {
-//            XCTAssertEqual(Platform.tvOS(.v16).isCurrent, false)
-//            XCTAssertEqual(Platform.tvOS(.v15).isCurrent, false)
-//            XCTAssertEqual(Platform.tvOS(.v14).isCurrent, false)
-//            XCTAssertEqual(Platform.tvOS(.v13).isCurrent, false)
-//        }
+        #else
+        XCTAssertEqual(Platform.tvOS(.v16).isCurrent, false)
+        XCTAssertEqual(Platform.tvOS(.v15).isCurrent, false)
+        XCTAssertEqual(Platform.tvOS(.v14).isCurrent, false)
+        XCTAssertEqual(Platform.tvOS(.v13).isCurrent, false)
+        #endif
     }
 }
